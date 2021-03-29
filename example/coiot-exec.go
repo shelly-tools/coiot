50d6a70dd771f9bf0d3affd25f68c17392740e68
Thorsten Eurich <thorsten@eurich.de>
2021-03-29 13:40:36 +0200

    Updated exec example

--- example/coiot-exec.go
+++ example/coiot-exec.go
@@ -19,21 +19,25 @@ func main() {
 	up := true
 	IpAddress := flag.String("ip", "224.0.1.187", "the Shellys ip address")
 	Interval := flag.Int("interval", 2000, "Interval to change the color")
+	loopcount := flag.Int("loops", 10, "Number of loops")
+	brightness := flag.Int("brightness", 100, "brightness")
+	gain := flag.Int("gain", 100, "gain")
 	Transition := flag.String("transition", "1000", "Interval to change the color")
+
 	Pcol := flag.String("pcol", "0,0,255,0", "primary color rbgw, default is 0,0,255,0")
 	Scol := flag.String("scol", "255,0,0,0", "secondary color in rgbw, default is 255,0,0,0")
 
 	flag.Parse()
 	// Simple infinite color change between blue and green every 5 seconds
-	for {
+	for i := 0; i < *loopcount; i++ {
 		var payload string
 
 		if up == true {
 			up = false
-			payload = `{"a":100,"i":[101,102,103,104,105,106,107,108],"v":[` + *Pcol + `,100,100,` + *Transition + `,1]}`
+			payload = `{"a":100,"i":[101,102,103,104,105,106,107,108],"v":[` + *Pcol + `,50,10,` + *Transition + `,1]}`
 		} else {
 			up = true
-			payload = `{"a":100,"i":[101,102,103,104,105,106,107,108],"v":[` + *Scol + `,100,100,` + *Transition + `,1]}`
+			payload = `{"a":100,"i":[101,102,103,104,105,106,107,108],"v":[` + *Scol + `,10,10,` + *Transition + `,1]}`
 		}
 		Id = Id + 1
 		req := coiot.Message{
